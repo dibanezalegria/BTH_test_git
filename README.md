7008962073cb1d0c6654aa12724cc9ee1d2020cc
Daniel Ibanez <dibanezalegria@yahoo.com>
2017-11-02 11:17:29 +0000

    Changing content

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the readme file
+=======================
+
+Project's description goes here.
---
+++ hello.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Test_Git</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <?php echo "Testing Git repository"; ?>
+    </body>
+</html>
